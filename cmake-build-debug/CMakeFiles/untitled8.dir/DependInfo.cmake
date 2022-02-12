fdbe9f1fa7bad25083045832f62cf689fc2a38a2
w1zZzard <semeon.froloff13@gmail.com>
2022-02-12 17:57:06 +0300

    .

--- cmake-build-debug/CMakeFiles/untitled8.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled8.dir/DependInfo.cmake
@@ -8,6 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/megam/CLionProjects/practice/libs/matrix.c" "C:/Users/megam/CLionProjects/practice/cmake-build-debug/CMakeFiles/untitled8.dir/libs/matrix.c.obj"
   "C:/Users/megam/CLionProjects/practice/libs/vector.c" "C:/Users/megam/CLionProjects/practice/cmake-build-debug/CMakeFiles/untitled8.dir/libs/vector.c.obj"
   "C:/Users/megam/CLionProjects/practice/main.c" "C:/Users/megam/CLionProjects/practice/cmake-build-debug/CMakeFiles/untitled8.dir/main.c.obj"
   )
